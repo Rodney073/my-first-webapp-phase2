d634b7dd8fa37a5816ec8d5e01aff89ba29e2bd3
Rodney073 <63658050+Rodney073@users.noreply.github.com>
2020-05-01 15:41:17 +0200

    JS - first steps

---
+++ strings.js
@@ -0,0 +1,70 @@
+/*$('.photoframe').append('<img id="bigpic" src="https://i.imgur.com/GNNcXpQ.jpg" alt="">');
+$('.textbox').append('<h1>So Cute!</h1>');
+$('.textbox').append('<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>');
+
+let data = {
+    photo: 'images/photo1.jpg',
+    title: 'My title',
+    description: 'What happened here, why is this a very nice image'
+  };*/
+  
+  //$('#photo').attr('src', data.photo);
+
+
+  let pic1 = {
+    photo:'https://static.boredpanda.com/blog/wp-content/uploads/2019/04/travelling-cuddling-stray-cats-istanbul-orin-fb12.png',
+    title: 'Cute1',
+    text: 'Neque porro quisquam est1'
+   };
+
+   let pic2 = {
+    photo:'https://m.blog.hu/ci/citydogs/image/kutya-macska_baratsag_10.jpg',
+    title: 'Cute2',
+    text: 'Neque porro quisquam est2'
+   };
+
+   let pic3 = {
+    photo:'https://hips.hearstapps.com/wdy.h-cdn.co/assets/15/41/1444244374-gettyimages-559292053.jpg',
+    title: 'Cute3',
+    text: 'Neque porro quisquam est3'
+   };
+
+   let pic4 = {
+    photo:'https://i.ytimg.com/vi/4V7uTRwnaaw/maxresdefault.jpg',
+    title: 'Cute4',
+    text: 'Neque porro quisquam est4'
+   };
+
+   let pic5 = {
+    photo:'https://i.imgur.com/GNNcXpQ.jpg',
+    title: 'Cute5',
+    text: 'Neque porro quisquam est5'
+   };
+
+   let pic6 = {
+    photo:'https://www.petspyjamas.com/uploads/2013/07/can-cats-and-dogs-be-friends-1.jpg',
+    title: 'Cute6',
+    text: 'Neque porro quisquam est6'
+   };
+
+
+
+let currentPhoto = 0;
+let imagesData = [pic1, pic2, pic3, pic4, pic5, pic6];
+/*$('#photo').attr('src', imagesData[currentPhoto].photo);
+$('#photo-title').append('<h1>'+imagesData[currentPhoto].title+'</h1>');
+$('#photo-description').append('<p>'+imagesData[currentPhoto].text+'</p>');*/
+
+
+let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+    $('#photo-title').append('<h1>'+imagesData[photoNumber].title+'</h1>');
+    $('#photo-description').append('<p>'+imagesData[photoNumber].text+'</p>');
+  }
+  
+  loadPhoto(currentPhoto);
+  
+  $('.right_arrow_frame').click(() => {
+    currentPhoto++;
+    loadPhoto(currentPhoto);
+  })
