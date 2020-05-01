923915ddc873df8d91d70e4c60e745ad1b833862
Rodney073 <63658050+Rodney073@users.noreply.github.com>
2020-05-01 18:58:44 +0200

    working thumbnails - ugly

--- strings.js
+++ strings.js
@@ -1,50 +1,43 @@
-/*$('.photoframe').append('<img id="bigpic" src="https://i.imgur.com/GNNcXpQ.jpg" alt="">');
-$('.textbox').append('<h1>So Cute!</h1>');
-$('.textbox').append('<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>');
-
-let data = {
-    photo: 'images/photo1.jpg',
-    title: 'My title',
-    description: 'What happened here, why is this a very nice image'
-  };*/
-
-//$('#photo').attr('src', data.photo);
-
-
 let pic1 = {
     photo: 'https://static.boredpanda.com/blog/wp-content/uploads/2019/04/travelling-cuddling-stray-cats-istanbul-orin-fb12.png',
     title: 'Cute1',
-    text: 'Neque porro quisquam est1'
+    text: 'Neque porro quisquam est1',
+    id: 'no1'
 };
 
 let pic2 = {
     photo: 'https://m.blog.hu/ci/citydogs/image/kutya-macska_baratsag_10.jpg',
     title: 'Cute2',
-    text: 'Neque porro quisquam est2'
+    text: 'Neque porro quisquam est2',
+    id: 'no2'
 };
 
 let pic3 = {
     photo: 'https://hips.hearstapps.com/wdy.h-cdn.co/assets/15/41/1444244374-gettyimages-559292053.jpg',
     title: 'Cute3',
-    text: 'Neque porro quisquam est3'
+    text: 'Neque porro quisquam est3',
+    id: 'no3'
 };
 
 let pic4 = {
     photo: 'https://i.ytimg.com/vi/4V7uTRwnaaw/maxresdefault.jpg',
     title: 'Cute4',
-    text: 'Neque porro quisquam est4'
+    text: 'Neque porro quisquam est4',
+    id: 'no4'
 };
 
 let pic5 = {
     photo: 'https://i.imgur.com/GNNcXpQ.jpg',
     title: 'Cute5',
-    text: 'Neque porro quisquam est5'
+    text: 'Neque porro quisquam est5',
+    id: 'no5'
 };
 
 let pic6 = {
     photo: 'https://www.petspyjamas.com/uploads/2013/07/can-cats-and-dogs-be-friends-1.jpg',
     title: 'Cute6',
-    text: 'Neque porro quisquam est6'
+    text: 'Neque porro quisquam est6',
+    id: 'no6'
 };
 
 
@@ -83,8 +76,65 @@ $('.left_arrow_frame').click(() => {
 
 
 imagesData.forEach((numofpic) => {
-    $('.tinypics').append('<img id="thumbnails" src="' + numofpic.photo + '">');
+    $('.tinypics').append('<img class="thumbnails" id="'+numofpic.id+'" src="' + numofpic.photo + '">');
 })
+
+
+$('#no1').click(() => {
+    $('h1').text('');
+    $('p').text('');
+    loadPhoto(currentPhoto=0);
+})
+
+
+$('#no2').click(() => {
+    $('h1').text('');
+    $('p').text('');
+    loadPhoto(currentPhoto=1);
+})
+
+$('#no3').click(() => {
+    $('h1').text('');
+    $('p').text('');
+    loadPhoto(currentPhoto=2);
+})
+
+
+$('#no4').click(() => {
+    $('h1').text('');
+    $('p').text('');
+    loadPhoto(currentPhoto=3);
+})
+
+$('#no5').click(() => {
+    $('h1').text('');
+    $('p').text('');
+    loadPhoto(currentPhoto=4);
+})
+
+$('#no6').click(() => {
+    $('h1').text('');
+    $('p').text('');
+    loadPhoto(currentPhoto=5);
+})
+
+
+
+
+
+/*
+
+photo.forEach((item, index) => {
+    $('#thumbnail-box').append(IDE ALT+7<div class="thumbnail"> <img class="thumbnail-image" src="${item}" id="${index}"> </div> IDE ALT+7);
+    
+    
+
+    for (let index = 0; index < images.length; index++) {
+        $thumb = '#pic' + index;
+        $($thumb).attr("src", "./images/"+images[index].file);
+    }
+
+*/
 
 
 
